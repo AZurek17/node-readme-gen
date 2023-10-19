41a46ceb9611a1fae72b1ce8f9dc17da46a0e3b4
Andy <84168944+AZurek17@users.noreply.github.com>
2023-10-18 17:16:28 -0700

    finished Psuedo Code

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+    .createPromptModule([
+        {
+            type
+        }
+    ])
+];
+//* add type, name, and message for each question
+//* add questions:  added readme sections (description, usage, license, etc)
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+//* use FS module to write data and fileName to readme file    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -16,4 +29,4 @@ init();
 
 //* use inquiter to display questios to user
 //* call generateMarkdown funcoin on the data collected
-//* take what is returned to us, then pass it to the writeToFil funciton
+//* take what is returned to us, then pass it to the writeToFile funciton
